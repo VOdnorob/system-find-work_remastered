d7e185cb35fb21bc11a76dc52db4ba88b1e7ce5f
Valentyn <valentyn.odnorob@gmail.com>
2024-03-30 11:00:45 +0100

    without validator methods

--- src/main/java/com/diploma/projectDiploma/services/WorkerService.java
+++ src/main/java/com/diploma/projectDiploma/services/WorkerService.java
@@ -18,12 +18,6 @@ public class WorkerService {
     private static final String EMAIL_REGEX = ".+@.+\\..+";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
-    public void validateEmail(String email) {
-        if (!EMAIL_PATTERN.matcher(email).matches()) {
-            throw new IllegalArgumentException("Bad format registered email");
-        }
-    }
-
     public void validationPesel(String pesel) {
 
         if (pesel.length() != 11) {
@@ -39,16 +33,7 @@ public class WorkerService {
 
     public Worker createWorker(Worker worker)
     {
-       // validateEmail(worker.getEmail());
-         validationPesel(worker.getPesel());
 
-        workerRepository.findByEmail(worker.getEmail())
-                .ifPresent(w -> {
-                    throw new ResponseStatusException(
-                            HttpStatus.BAD_REQUEST,
-                            "Worker with " + worker.getEmail() + " already registered"
-                    );
-                });
         return workerRepository.save(worker);
     }
 
